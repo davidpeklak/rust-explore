bf13e1162f9bb7d63a5bebde7c3703b501cd1cf2
David Peklak <dpeklak@gmail.com>
2017-01-31 20:31:54 +0100

    make List generic

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,16 @@ use std::fmt;
 
 fn main() {
 
-    enum List <'a> {
+    enum List <'a, T: 'a> {
         Nil,
-        Cons { head: i32, tail: &'a List<'a> } 
+        Cons { head: T, tail: &'a List<'a, T> } 
     }
 
-    fn prnt(l: &List) {
-        match l {
-            &List::Nil => println!("Nil"),
-            &List::Cons { head: h, tail: t } =>  { 
-               println!("{}", h);
-               prnt(t)
-            }
-        }
-    }
-
-    impl<'a> fmt::Display for List<'a> {
+    impl<'a, T: fmt::Display> fmt::Display for List<'a, T> {
         fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
             match self {
                 &List::Nil => write!(f, "Nil"),
-                &List::Cons{ head: h, tail: t } => {
+                &List::Cons{ head: ref h, tail: t } => {
                     write!(f, "{} :: ", h);
                     t.fmt(f)
                 }
@@ -32,7 +22,11 @@ fn main() {
     let n = List::Nil;
     let n1 = List::Cons{head: 2, tail: &n};
 
-    // prnt(&n1);
-    println!("The winner is: {}", n1);
+    println!("The i32 List: {}", n1);
+
+    let s = List::Nil;
+    let s1 = List::Cons{head: "Hallo", tail: &s};
+
+    println!("The str List: {}", s1);
 }
 
