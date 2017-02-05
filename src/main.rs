cd19767ee00b279954591b94590c912fb1f0eede
David Peklak <dpeklak@gmail.com>
2017-02-05 08:29:20 +0100

    dynamic dispatch

--- src/main.rs
+++ src/main.rs
@@ -68,22 +68,28 @@ fn main() {
         }}      
     }
 
-    fn stat_dispatch<T: Foo>(t: T) {
-        println!("{}", t.to_a_string())
+    fn stat_dispatch<T: Foo>(t: &T) {
+        println!("stat: {}", t.to_a_string())
+    }
+
+    fn dyn_dispatch(t: &Foo) {
+        println!("dyn: {}", t.to_a_string())
     }
 
     let n1 = List!(2, 3, 5, 8, 13, 21, 34, 55, 89, 144);
 
-    stat_dispatch(n1);
+    stat_dispatch(&n1);
+    dyn_dispatch(&n1);
 
     let o1 = Opt::Some(3);
 
-    stat_dispatch(o1);
+    stat_dispatch(&o1);
+    dyn_dispatch(&o1);
 
     let o2: Opt<i32>  = Opt::None;
 
-    stat_dispatch(o2);
-
+    stat_dispatch(&o2);
+    dyn_dispatch(&o2);
 
 }
 
