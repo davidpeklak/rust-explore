f4d2928c14925eb56c0efcba4925bfc90384f871
David Peklak <dpeklak@gmail.com>
2017-02-13 21:15:15 +0100

    read messages from file

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,25 +6,35 @@ use std::io;
 use std::fs::File;
 
 fn main() {
+    {
+        let mut file = File::create("target/foo.txt")
+            .expect("Could not open file");
 
-    let mut file = File::create("target/foo.txt")
-        .expect("Could not open file");
+        loop {
+            println!("Enter a message");
 
-    loop {
-        println!("Enter a message");
+            let mut input = String::new();
 
-        let mut input = String::new();
+            io::stdin().read_line(&mut input)
+                .expect("Failed to read line");
 
-        io::stdin().read_line(&mut input)
-            .expect("Failed to read line");
+            let trimmed_input = input.trim();
 
-        if input.trim().len() == 0 {
-            break;
+            if trimmed_input.len() == 0 {
+                break;
+            }
+
+            write_to_file(&mut file, &trimmed_input)
         }
-
-        write_to_file(&mut file, &mut input)
     }
+    {
+        let mut file = File::open("target/foo.txt").unwrap();
 
+        loop {
+            let msg = read_from_file(&mut file);
+            println!("{}", msg);
+        }
+    }
 }
 
 fn write_to_file(file: &mut File, msg: &str) {
@@ -38,26 +48,27 @@ fn write_to_file(file: &mut File, msg: &str) {
 
     println!("write length: {}", length);
 
-    file.write_all(&ba).unwrap();
-    file.write_all(msg.as_bytes()).unwrap();
+    file.write_all(&ba)
+        .expect("Could not right message length to file");
+    file.write_all(msg.as_bytes())
+        .expect("Could not right message to file");
 }
 
-fn read_from_file() -> String {
-    use std::fs::File;
+fn read_from_file(file: &mut File) -> String {
     use std::io::Read;
     use std::str::from_utf8;
     use byteorder::{ByteOrder, BigEndian};
 
-    let mut f = File::open("target/foo.txt").unwrap();
-
     let mut ba = [0u8; 4];
-    f.read_exact(&mut ba).unwrap();
+    file.read_exact(&mut ba)
+        .expect("Could not read message length from file");
 
     let length = BigEndian::read_u32(&mut ba) as usize;
     println!("read length: {}", length);
 
     let mut buf = vec![0; length].into_boxed_slice();
-    f.read_exact(&mut buf).unwrap();
+    file.read_exact(&mut buf)
+        .expect("Could not read message from file");
 
     let s = from_utf8(&buf).unwrap();
 
