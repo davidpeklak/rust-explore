56a65650754d78c8b3d2c6ebca6af8c2313e8568
David Peklak <dpeklak@gmail.com>
2017-06-19 18:22:50 +0200

    use associated type in Future trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use std::marker::Sized;
 
-pub trait Future<T>
-where Self: Sized {
+pub trait Future
+    where Self: Sized {
+    type T;
     fn map<U, M>(self, mf: M) -> Map<Self, M>
-        where M: Fn(T) -> U;
+        where M: Fn(Self::T) -> U;
 }
 
-pub struct FutureImpl<T, F>
-    where F: Fn() -> T {
+pub struct FutureImpl<F> {
     pub fun: F
 }
 
@@ -16,17 +16,19 @@ pub struct Map<F, M> {
     pub mf: M
 }
 
-impl<T, F> Future<T> for FutureImpl<T, F>
+impl<T, F> Future for FutureImpl<F>
     where F: Fn() -> T {
+    type T = T;
     fn map<U, M>(self, mf: M) -> Map<Self, M>
         where M: Fn(T) -> U {
         Map { fut: self, mf: mf }
     }
 }
 
-impl<T, F, M, U> Future<U> for Map<F, M>
-    where F: Future<T>,
+impl<T, F, M, U> Future for Map<F, M>
+    where F: Future<T=T>,
           M: Fn(T) -> U {
+    type T = U;
     fn map<V, N>(self, mf: N) -> Map<Self, N>
         where N: Fn(U) -> V {
         Map { fut: self, mf: mf }
