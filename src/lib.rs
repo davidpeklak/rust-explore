3be40663b49e23e86c43441604b89b5415dbdbcc
David Peklak <dpeklak@gmail.com>
2017-06-19 08:02:07 +0200

    use PhantomData T in Map

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use std::marker::Sized;
+use std::marker::PhantomData;
 
 pub trait Future<T>
 where Self: Sized {
-    fn map<U, M>(self, mf: M) -> Map<Self, M>
+    fn map<U, M>(self, mf: M) -> Map<T, Self, M>
         where M: Fn(T) -> U;
 }
 
@@ -11,24 +12,26 @@ pub struct FutureImpl<T, F>
     pub fun: F
 }
 
-pub struct Map<F, M> {
+pub struct Map<T, F, M>
+    where F: Future<T> {
     pub fut: F,
-    pub mf: M
+    pub mf: M,
+    phantom_t: PhantomData<T>
 }
 
 impl<T, F> Future<T> for FutureImpl<T, F>
     where F: Fn() -> T {
-    fn map<U, M>(self, mf: M) -> Map<Self, M>
+    fn map<U, M>(self, mf: M) -> Map<T, Self, M>
         where M: Fn(T) -> U {
-        Map { fut: self, mf: mf }
+        Map { fut: self, mf: mf, phantom_t: PhantomData }
     }
 }
 
-impl<T, F, M, U> Future<U> for Map<F, M>
+impl<T, F, M, U> Future<U> for Map<T, F, M>
     where F: Future<T>,
           M: Fn(T) -> U {
-    fn map<V, N>(self, mf: N) -> Map<Self, N>
+    fn map<V, N>(self, mf: N) -> Map<U, Self, N>
         where N: Fn(U) -> V {
-        Map { fut: self, mf: mf }
+        Map { fut: self, mf: mf, phantom_t: PhantomData }
     }
 }
