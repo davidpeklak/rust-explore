7f8515542169c71584b13cb92befffe7a2bda404
David Peklak <dpeklak@gmail.com>
2017-06-22 12:47:35 +0200

    implement Execute for Future

--- src/exec.rs
+++ src/exec.rs
@@ -1,4 +1,15 @@
+use super::future::FutureImpl;
+
 pub trait Execute {
     type T;
     fn run_sync(&self) -> Self::T;
+}
+
+impl<T, F> Execute for FutureImpl<F>
+    where F: Fn() -> T {
+    type T = T;
+
+    fn run_sync(&self) -> Self::T {
+        (self.fun)()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 extern crate futexp;
 
 use futexp::future::*;
+use futexp::exec::Execute;
 
 fn main() {
     let fut = future(|| 3);
     let fut = fut.map(|x| x + 2);
     let fut = fut.map(|x| x * 2);
     let fut = fut.map(|x| x - 3);
+
+    let fut = future(|| {println!("Yes!")});
+
+    fut.run_sync();
+    fut.run_sync();
+    fut.run_sync();
 }
