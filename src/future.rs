a1eec9157b09a57a31e04d8e202a34a4f101200b
David Peklak <dpeklak@gmail.com>
2017-06-22 07:21:51 +0200

    add documentation

--- src/future.rs
+++ src/future.rs
@@ -1,13 +1,37 @@
 use std::marker::Sized;
 
+/// a trait that represents a computation that eventually will yield a value of type `T`
 pub trait Future
     where Self: Sized {
+    /// the type of the value that the future will eventually yield
     type T;
+
+    /// maps the `Future` with the function `fun`
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let f = future(|| 3);
+    /// let f = f.map(|x| x + 2);
+    /// ```
     fn map<U, M>(self, mf: M) -> Map<Self, M>
         where M: Fn(Self::T) -> U {
         Map { fut: self, mf: mf }
     }
 }
+
+/// creates a `Future` from the function `fun`
+///
+/// # Examples
+///
+/// ```
+/// let f = future(|| 3);
+/// ```
+pub fn future<T, F>(fun: F) -> FutureImpl<F>
+    where F: Fn() -> T {
+    FutureImpl { fun: fun }
+}
+
 
 pub struct FutureImpl<F> {
     pub fun: F
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate futexp;
 use futexp::future::*;
 
 fn main() {
-    let fut = FutureImpl { fun: || 3 };
+    let fut = future(|| 3);
     let fut = fut.map(|x| x + 2);
     let fut = fut.map(|x| x * 2);
     let fut = fut.map(|x| x - 3);
